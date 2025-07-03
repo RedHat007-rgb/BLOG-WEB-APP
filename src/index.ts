b6774a92f60eeeb4c26b0066267a2a423319f491
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-07-03 11:13:25 +0530

    implemented connect function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("Blog-WEB-APP.");
+import express from "express";
+import dotenv from "dotenv";
+import { dbConnect } from "./db";
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+const app = express();
+if (!process.env.PG_URL) {
+  throw new Error("PLease check the connection string");
+}
+const PgUrl: any = process.env.PG_URL;
+
+const connect = async () => {
+  try {
+    await dbConnect(PgUrl);
+    app.listen(PORT, () => {
+      console.log(`connected to PORT ${PORT}`);
+    });
+  } catch (error) {
+    console.log("SERVER ERROR...");
+  }
+};
+
+connect();
